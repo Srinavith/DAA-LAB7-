3b621fabeaea85308df3c8cc0133d8b13e495fc0
srinavith <srinavithsrinath3347@gmail.com>
2025-02-17 11:17:04 +0530

    lolipop

--- q1.c
+++ q1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+<<<<<<< HEAD
 struct node
 {
     int info;
@@ -48,3 +49,90 @@ int main()
     printf("Number of nodes in tree 3 = %d/n",countnodes(root));
     return 0;
 }
+=======
+
+#define MAX 100
+
+int adj[MAX][MAX], visited[MAX], stack[MAX], top = -1;
+int in_degree[MAX];
+int n; // Number of vertices
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+    stack[++top] = v;
+}
+
+void topologicalSortDFS() {
+    for (int i = 0; i < n; i++) visited[i] = 0;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i])
+            dfs(i);
+    }
+    printf("Topological Sort (DFS method): ");
+    while (top >= 0) {
+        printf("%d ", stack[top--]);
+    }
+    printf("\n");
+}
+void topologicalSortSourceRemoval() {
+    int queue[MAX], front = 0, rear = -1;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        in_degree[i] = 0;
+        for (int j = 0; j < n; j++) {
+            if (adj[j][i]) {
+                in_degree[i]++;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (in_degree[i] == 0) {
+            queue[++rear] = i;
+        }
+    }
+    printf("Topological Sort (Source Removal method): ");
+    while (front <= rear) {
+        int v = queue[front++];
+        printf("%d ", v);
+        count++;
+        for (int i = 0; i < n; i++) {
+            if (adj[v][i]) {
+                in_degree[i]--;
+                if (in_degree[i] == 0) {
+                    queue[++rear] = i;
+                }
+            }
+        }
+    }
+    printf("\n");
+    if (count != n) {
+        printf("Graph has a cycle! Topological sorting not possible.\n");
+    }
+}
+
+int main() {
+    int edges, u, v;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter the number of edges: ");
+    scanf("%d", &edges);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+    printf("Enter the edges (start end):\n");
+    for (int i = 0; i < edges; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+    }
+    topologicalSortDFS();
+    topologicalSortSourceRemoval();
+    return 0;
+}
+>>>>>>> 8614423 (message)
--- q2.c
+++ q2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+<<<<<<< HEAD
 void swap(int* a, int* b) { 
     int t = *a; 
     *a = *b; 
@@ -45,3 +46,58 @@ int main() {
 
     return 0; 
 }
+=======
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left, *right;
+};
+
+struct node *cca(struct node *root, int node_1, int node_2) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    if (root->data > node_1 && root->data > node_2) {
+        return cca(root->left, node_1, node_2);
+    }
+
+    if (root->data < node_1 && root->data < node_2) {
+        return cca(root->right, node_1, node_2);
+    }
+
+    return root;
+}
+
+struct node *newNode(int data) {
+    struct node *p = (struct node*)malloc(sizeof(struct node));
+    p->data = data;
+    p->left = p->right = NULL;
+    return p;
+}
+
+int main() {
+    struct node *root = newNode(20);
+    root->left = newNode(8);
+    root->right = newNode(22);
+    root->left->left = newNode(4);
+    root->left->right = newNode(12);
+    root->left->right->left = newNode(10);
+    root->left->right->right = newNode(14);
+
+    int node_1 = 10, node_2 = 14;
+    struct node *t = cca(root, node_1, node_2);
+    printf("CCA of %d and %d is %d \n", node_1, node_2, t->data);
+
+    node_1 = 14, node_2 = 8;
+    t = cca(root, node_1, node_2);
+    printf("CCA of %d and %d is %d \n", node_1, node_2, t->data);
+
+    node_1 = 10, node_2 = 22;
+    t = cca(root, node_1, node_2);
+    printf("CCA of %d and %d is %d \n", node_1, node_2, t->data);
+
+    return 0;
+}
+>>>>>>> 8614423 (message)
